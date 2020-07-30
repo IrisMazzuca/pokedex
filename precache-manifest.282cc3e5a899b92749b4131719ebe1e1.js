70146d459f9d7b09716f886928c4903617036d6f
Iris Mazzuca <irismazzuca@gmail.com>
2020-07-30 00:36:34 -0300

    Updates

---
+++ precache-manifest.282cc3e5a899b92749b4131719ebe1e1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "761d85e1f9aebfee67cbf79474a3863d",
+    "url": "/pokedex/index.html"
+  },
+  {
+    "revision": "cd85fadfa294541b5590",
+    "url": "/pokedex/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "a988eb1f80032c123644",
+    "url": "/pokedex/static/css/main.6bbcd38b.chunk.css"
+  },
+  {
+    "revision": "cd85fadfa294541b5590",
+    "url": "/pokedex/static/js/2.dd9d8878.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pokedex/static/js/2.dd9d8878.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a988eb1f80032c123644",
+    "url": "/pokedex/static/js/main.a214c7da.chunk.js"
+  },
+  {
+    "revision": "0f0f5ad1ceed24d8d4c8",
+    "url": "/pokedex/static/js/runtime-main.23b75adc.js"
+  }
+]);
